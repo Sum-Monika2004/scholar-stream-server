8b4013f027665e48950422c1501b7250ff507b16
Sum-Monika2004 <13monika04@gmail.com>
2026-02-15 02:55:11 +0600

    stripe integration in payment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const admin = require("firebase-admin");
 // const serviceAccount = require("./serviceAccountKey.json");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const port = process.env.PORT || 3000;
 
@@ -61,20 +62,23 @@ async function run() {
 
     const db = client.db("scholar_stream");
     const scholarshipsCollection = db.collection("scholarships");
+    const reviewsCollection = db.collection("reviews");
+    const applicationsCollection = db.collection("applications");
 
     // scholarships api
 
     app.get("/all-scholarships", async (req, res) => {
       const result = await scholarshipsCollection.find().toArray();
-      console.log(result);
-
       res.send(result);
     });
 
+    // single scholarship
+
     app.get("/all-scholarships/:id", verifyToken, async (req, res) => {
       const { id } = req.params;
-      // console.log(id);
-
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid ID" });
+      }
       const result = await scholarshipsCollection.findOne({
         _id: new ObjectId(id),
       });
@@ -95,8 +99,6 @@ async function run() {
         })
         .limit(6)
         .toArray();
-      console.log(result);
-
       res.send(result);
     });
 
@@ -131,7 +133,37 @@ async function run() {
       res.send(result);
     });
 
-    //delete
+    // scholarship filter
+
+    app.get("/filter", async (req, res) => {
+      const { country, category } = req.query;
+
+      let query = {};
+      if (country) {
+        query.country = country;
+      }
+      if (category) {
+        query.category = category;
+      }
+      const result = await scholarshipsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // scholarship sort
+
+    app.get("/sort", async (req, res) => {
+      const { order } = req.query;
+      const sortOrder = order === "desc" ? -1 : 1;
+      const result = await scholarshipsCollection
+        .find()
+        .sort({ applicationFees: sortOrder })
+        .toArray();
+      console.log(result);
+
+      res.send(result);
+    });
+
+    //delete sch
 
     app.delete("/all-scholarships/:id", async (req, res) => {
       const { id } = req.params;
@@ -146,6 +178,127 @@ async function run() {
         result,
       });
     });
+
+    // reviews stored
+
+    app.post("/reviews", verifyToken, async (req, res) => {
+      const review = req.body;
+
+      review.reviewDate = new Date().toISOString().split("T")[0];
+      const result = await reviewsCollection.insertOne(review);
+      res.send({
+        success: true,
+        insertedId: result.insertedId,
+        ...review,
+      });
+    });
+
+    // get reviews
+
+    app.get("/reviews/:scholarshipId", async (req, res) => {
+      const { scholarshipId } = req.params;
+
+      const result = await reviewsCollection
+        .find({ scholarshipId })
+        .sort({ reviewDate: -1 })
+        .toArray();
+
+      res.send(result);
+    });
+
+    // get all reviews
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // delete reviews
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const { id } = req.params;
+
+      const result = await reviewsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send({ success: true });
+    });
+
+    // payment related apis
+
+    app.post("/create-payment-session", verifyToken, async (req, res) => {
+      const paymentInfo = req.body;
+      const amount =
+        (Number(paymentInfo.applicationFees || 0) +
+          Number(paymentInfo.tuitionFees || 0) +
+          Number(paymentInfo.serviceCharge || 0)) *
+        100;
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        customer_email: paymentInfo.userEmail,
+        line_items: [
+          {
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.scholarshipName,
+              },
+            },
+
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.userEmail,
+        metadata: {
+          scholarshipId: paymentInfo.scholarshipId,
+          scholarshipName: paymentInfo.scholarshipName,
+          universityName: paymentInfo.universityName,
+        },
+        mode: "payment",
+        success_url: `${process.env.SITE_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SITE_DOMAIN}/payment-failure?session_id={CHECKOUT_SESSION_ID}`,
+      });
+      console.log(session);
+      res.send({ url: session.url });
+    });
+
+    // payment success
+
+    app.get("/payment-success/:sessionId", async (req, res) => {
+      const { sessionId } = req.params;
+
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+      res.send({
+        scholarshipId: session.metadata.scholarshipId,
+        email: session.customer_email,
+        amount: session.amount_total / 100,
+        scholarshipName: session.metadata.scholarshipName,
+        universityName: session.metadata.universityName,
+      });
+    });
+
+    // payment failure
+
+    app.get("/payment-failure/:sessionId", async (req, res) => {
+      const { sessionId } = req.params;
+
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+      res.send({
+        scholarshipId: session.metadata.scholarshipId,
+        email: session.customer_email,
+        amount: session.amount_total / 100,
+        status: session.payment_status,
+        scholarshipName: session.metadata.scholarshipName,
+      });
+    });
+
+    ////////////////////////////////////////////////////////////////////
 
     await client.db("admin").command({ ping: 1 });
     console.log(
